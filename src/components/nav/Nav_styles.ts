f3848e1ac302c4e450bec0ca3374c19c7a1a53eb
Anton-Osip <osipchika@gmail.com>
2024-06-28 22:37:17 +0300

    feat: add refactoring styles

--- src/components/nav/Nav_styles.ts
+++ src/components/nav/Nav_styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {font} from '../../styles/Common'
+import {NavLink} from "react-router-dom";
+import {theme} from "../../styles/Theme";
 
 const Nav = styled.nav`
     display: flex;
@@ -16,13 +18,37 @@ const NavList = styled.ul`
     gap: 40px;
 `
 
-const Link = styled.a`
+const Link = styled (NavLink) `
     ${font({
         weight: 400,
         Fmax: 24,
         Fmin: 24,
         lineHeight: 1.3,
     })}
+    position: relative;
+    transition: ${theme.animations.transition};
+
+    &:before {
+        content: '';
+        display: inline-flex;
+        position: absolute;
+        bottom: -8px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 2px;
+        background-color: ${theme.colors.secondaryColor};
+        transform: rotateY(90deg);
+        transition: ${theme.animations.transition};
+    }
+
+    .active&:before {
+        transform: rotateY(0deg);
+    }
+
+    &:hover {
+        color: ${theme.colors.hoverColor};
+    }
 
 `
 
--- src/components/search/Search_styles.ts
+++ src/components/search/Search_styles.ts
@@ -1,23 +1,56 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {font} from "../../styles/Common";
 import search from '../../assets/image/searchIcon.svg'
+import {theme} from "../../styles/Theme";
+
 const Form = styled.form`
     max-width: 496px;
     flex-grow: 1;
+    display: flex;
+    position: relative;
 `
 
-const Input = styled.input`
+const Input = styled.input<{ isSuggests?: boolean }>`
     outline: none;
     border: 0;
     border-radius: 8px;
-    background: rgb(56, 58, 60);
-    background-image: url(${search});
-    background-repeat: no-repeat;
-    background-position: 16px center;
+    background:${theme.colors.secondaryBg} url(${search}) no-repeat 16px center;
     padding: 16px 16px 16px 52px;
     width: 100%;
     ${font({Fmax: 18, Fmin: 18})}
+
+    ${props => props.isSuggests && css<{ isSuggests?: boolean }>`
+        border-radius: 8px 0 0 8px;
+    `}
+`
+
+const Button = styled.button`
+    height: 54px;
+    width: 54px;
+    background: ${theme.colors.secondaryBg};
+    border-radius: 0 8px 8px 0;
+    cursor: pointer;
+    position: relative;
+
+    &:after, &:before {
+        content: '';
+        display: inline-block;
+        height: 2px;
+        background-color: ${theme.colors.fontColor};
+        width: 20px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+    }
+
+    &:after {
+        transform: translateX(-50%) rotate(45deg);
+    }
+
+    &:before {
+        transform: translateX(-50%) rotate(-45deg);
+    }
 `
 
 
-export const s = {Form, Input}
+export const s = {Form, Input, Button}
